a0faf9e99d9c9778ffc244e7e222d7da081fa59e
ravindra-agnihotri <51674492+ravindra-agnihotri@users.noreply.github.com>
2021-08-15 07:34:11 +0530

    committed legger

--- src/main/java/backbone/DriverInvoker.java
+++ src/main/java/backbone/DriverInvoker.java
@@ -1,2 +1,28 @@
-package backbone;public class DriverInvoker {
+package backbone;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverInvoker {
+
+    private static WebDriver driver;
+    private static DriverInvoker driverInvoker;
+
+    private DriverInvoker() {}
+
+    public static WebDriver getInstance() {
+        if (driver==null){
+            return driverInvoker.getDriver();
+        }
+        else return driver;
+    }
+
+
+    public WebDriver getDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        return driver;
+    }
 }
